82c4c49955c80fd624e9e4bb527798ffbe07f3ba
Artyom freezyng <tema9651@yandex.ru>
2021-07-04 21:05:17 +0300

    added put, delete, post

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var artists = [
     {
@@ -26,9 +30,38 @@ app.get('/artists', function(req, res){
 })
 
 app.get('/artists/:id', function(req, res){
-    res.send('test');
+    console.log(req.params);
+    var artist = artists.find(function(artist){
+        return artist.id === Number(req.params.id);
+    })
+    res.send(artist);
+})
+
+app.post('/artists', function(req, res){
+    var artist = {
+        id: Date.now(),
+        name: req.body.name
+    }
+
+    artists.push(artist);
+    res.send(artists);
+})
+
+app.put('/artists/:id', function(req, res){
+    var artist = artists.find(function(artist){
+        return artist.id === Number(req.params.id);
+    })
+    artist.name = req.body.name;
+    res.sendStatus(200);
+})
+
+app.delete('/artists/:id', function(req, res){
+    artists = artists.filter(function(artist){
+        return artist.id !== Number(req.params.id);
+    })
+    res.sendStatus(200);
 })
 
 app.listen(3012, function(){
-    console.log('API app started')
+    console.log('API app started');
 })
